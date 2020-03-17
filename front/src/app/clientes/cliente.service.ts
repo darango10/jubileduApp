cb353cd0fad4663bc867d5282ca496360f3417f9
Darango <arangomonsalve.daniel@gmail.com>
2020-03-16 19:11:32 -0500

    Ahora es posible registrar Usuarios

--- front/src/app/clientes/cliente.service.ts
+++ front/src/app/clientes/cliente.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {CLIENTES} from './clientes.json';
 import {Cliente} from './cliente';
 import {Observable, of} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +10,16 @@ import {HttpClient} from '@angular/common/http';
 export class ClienteService {
 
   private urlEndpoint = 'http://localhost:8080/api/clientes';
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
   constructor(private http: HttpClient) {
   }
 
   getClientes(): Observable<Cliente[]> {
     return this.http.get<Cliente[]>(this.urlEndpoint);
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndpoint, cliente, {headers: this.httpHeaders});
   }
 }
--- front/src/app/registro/registro.component.ts
+++ front/src/app/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {Cliente} from '../clientes/cliente';
+import {ClienteService} from '../clientes/cliente.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -7,11 +10,18 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
   titulo = 'Sign Up';
+  cliente: Cliente = new Cliente();
+  formRegistro: any;
 
-  constructor() {
+  constructor(private clienteService: ClienteService, private router: Router) {
   }
 
   ngOnInit(): void {
   }
 
+  create() {
+    this.clienteService.create(this.cliente).subscribe(
+      response => this.router.navigate(['/clientes']));
+    console.log(this.cliente);
+  }
 }
