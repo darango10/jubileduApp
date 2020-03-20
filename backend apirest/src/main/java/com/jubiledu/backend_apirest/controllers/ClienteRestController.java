8ff0187316d244fc2ca321598b32f425e71d9a4f
Darango <arangomonsalve.daniel@gmail.com>
2020-03-19 21:53:46 -0500

    Manejo de Errores BackEnd

--- backend apirest/src/main/java/com/jubiledu/backend_apirest/controllers/ClienteRestController.java
+++ backend apirest/src/main/java/com/jubiledu/backend_apirest/controllers/ClienteRestController.java
@@ -3,10 +3,14 @@ package com.jubiledu.backend_apirest.controllers;
 import com.jubiledu.backend_apirest.models.entity.Cliente;
 import com.jubiledu.backend_apirest.models.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -22,38 +26,95 @@ public class ClienteRestController {
     }
 
     @GetMapping("/clientes/registro/{id}")
-    public Cliente show(@PathVariable Long id) {
-        return clienteService.findById(id);
+    public ResponseEntity<?> show(@PathVariable Long id) {
+        Cliente cliente = null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            cliente = clienteService.findById(id);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al realizar la consulta en la base de datos.");
+            response.put("mensaje", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if (cliente == null) {
+            response.put("mensaje", "El cliente con ID: " + id + " no existe.");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(cliente, HttpStatus.OK);
+
     }
 
     @PostMapping("/clientes")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Cliente create(@RequestBody Cliente cliente) {
-        return clienteService.save(cliente);
+    public ResponseEntity<?> create(@RequestBody Cliente cliente) {
+        Cliente clienteNew = null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            clienteNew = clienteService.save(cliente);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al crear el cliente en la base de datos.");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        response.put("mensaje", "El cliente ha sido creado con exito.");
+        response.put("cliente", clienteNew);
+        return new ResponseEntity<Map>(response, HttpStatus.CREATED);
     }
 
     @PutMapping("/clientes/registro/{id}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+    public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable Long id) {
+
+        Cliente clienteUpdated = null;
         Cliente clienteActual = clienteService.findById(id);
+        Map<String, Object> response = new HashMap<>();
 
-        clienteActual.setCedula(cliente.getCedula());
-        clienteActual.setNombre(cliente.getNombre());
-        clienteActual.setApellido(cliente.getApellido());
-        clienteActual.setEmail(cliente.getEmail());
-        clienteActual.setPassword(cliente.getPassword());
-        clienteActual.setFecha(cliente.getFecha());
-        clienteActual.setTelefono(cliente.getTelefono());
-        clienteActual.setGenero(cliente.getGenero());
-        clienteActual.setSemanas(cliente.getSemanas());
-        clienteActual.setSalario(cliente.getSalario());
+        if (clienteActual == null) {
+            response.put("mensaje", "El cliente con ID: " + id + " no existe.");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
 
-        return clienteService.save(clienteActual);
+        try {
+
+            clienteActual.setCedula(cliente.getCedula());
+            clienteActual.setNombre(cliente.getNombre());
+            clienteActual.setApellido(cliente.getApellido());
+            clienteActual.setEmail(cliente.getEmail());
+            clienteActual.setPassword(cliente.getPassword());
+            clienteActual.setFecha(cliente.getFecha());
+            clienteActual.setTelefono(cliente.getTelefono());
+            clienteActual.setGenero(cliente.getGenero());
+            clienteActual.setSemanas(cliente.getSemanas());
+            clienteActual.setSalario(cliente.getSalario());
+
+            clienteUpdated = clienteService.save(clienteActual);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al editar el cliente en la base de datos.");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje", "El cliente ha sido actualizado con exito.");
+        response.put("cliente", clienteUpdated);
+        return new ResponseEntity<Map>(response, HttpStatus.CREATED);
+
     }
 
     @DeleteMapping("clientes/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id) {
-        clienteService.delete(id);
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+
+        Map<String, Object> response = new HashMap<>();
+        try {
+            clienteService.delete(id);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al eliminar el cliente en la base de datos.");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje", "El cliente ha sido eliminado con exito.");
+        return new ResponseEntity<Map>(response, HttpStatus.OK);
+
+
     }
 }
